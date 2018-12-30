315c5d6d794b2b31fa28a50b860ee891c5275a28
Saar Weitzman <saarw93@gmail.com>
2018-12-30 17:20:26 +0200

    Update GenericHashTable.c

--- GenericHashTable.c
+++ GenericHashTable.c
@@ -1,13 +1,12 @@
 /**Author: Saar Weitzman
- * I.D: 204175137
- * Date: 13.12.18
+ * Date: 15.12.18
+ * Hash Table Implementation
  * **/
-
 
 #include "GenericHashTable.h"
 
 
-/***method to find the length of the list in a certain index of the hash table*/
+/** method to find the length of the list in a certain index of the hash table **/
 int list_length(Object* object)
 {
     if (object == NULL)
@@ -23,13 +22,13 @@ int list_length(Object* object)
 }
 
 
-/**method that gets a pointer to object which need to be added to the end of the list of "index" **/
+/** method that gets a pointer to object which need to be added to the end of the list of "index" **/
 void add_object_to_list(Table* table, Object* ptr_new_object, int index)
 {
-    Object* object_ptr = table->arr[index]->next;
+    Object* object_ptr = table->arr[index];
     if(object_ptr == NULL)  //there are no objects in the list of "index"
     {
-        table->arr[index]->next = ptr_new_object;
+        table->arr[index] = ptr_new_object;
         return;
     }
 
@@ -42,15 +41,15 @@ void add_object_to_list(Table* table, Object* ptr_new_object, int index)
 }
 
 
-/**method to check if there is still place in the list of the input index. It returns the index where the list is not full. If the list or lists
- * are full , it returns -1**/
+/** method to check if there is still place in the list of the input index. It returns the index where the list is not full. If the list or lists
+ * are full , it returns -1 **/
 int check_for_place(Table* table, int index)
 {
     Object* object_ptr = NULL;
     int i;
     for (i = index; i < index+table->d; i++)
     {
-        object_ptr = table->arr[i]->next; //pointer to move on the list
+        object_ptr = table->arr[i];   //pointer to move on the list
         int listLength = list_length(object_ptr);
         if (listLength < table->listLength)
             return i;
@@ -103,6 +102,12 @@ Table* createTable(int size, int dType, int listLength)
         return NULL;
     }
 
+    if (dType != INT_TYPE && dType != STR_TYPE)
+    {
+        perror("Error: The input of the dType is invalid\n");
+        return NULL;
+    }
+
 
     Table* table_ptr = (Table*)malloc(sizeof(Table));
     if(table_ptr == NULL)
@@ -120,7 +125,7 @@ Table* createTable(int size, int dType, int listLength)
     int i;
     for (i = 0; i < table_ptr->original_size; i++)
     {
-        table_ptr->arr[i] = createObject(NULL);
+        table_ptr->arr[i] = NULL;
     }
     return table_ptr;
 }
@@ -131,14 +136,21 @@ Table* createTable(int size, int dType, int listLength)
 * On success, the function returns the array index of the added data, otherwise, it return -1.
 */
 /**1.implement the hash function and gets from it the index that the new object is need to go to
- * 2.check if the list in the index of the hash table is not full before adding it to the list. If full-> duplicate the table
+ * 2.check if the list in the index of the hash table is not full before adding it to the list. If full-> duplicate the table and add object
+ * in the right place
  *  */
 int add(Table* table, void* data)
 {
     if (table == NULL)
     {
         perror("Error: The input pointer of the table equals to NULL\n");
-        return -1;
+        return ERR;
+    }
+
+    if (data == NULL)
+    {
+        perror("Error: Try to add a NULL value to the table");
+        return ERR;
     }
 
     int index = 0;
@@ -146,9 +158,9 @@ int add(Table* table, void* data)
 
     if (table->dType == STR_TYPE)
     {
-        char* str_data = (char*)malloc(sizeof(data)+1 ); //strlen(data)+1
+        char* str_data = (char*)malloc(sizeof(data)+1 );
         if (str_data == NULL)
-            return -1;
+            return ERR;
         strcpy(str_data, (char*)data);
 
         ptr_new_object = createObject(str_data);
@@ -161,53 +173,45 @@ int add(Table* table, void* data)
         ptr_new_object = createObject(data);
     }
 
-    if(table->arr[index]->next == NULL)  //there are no objects in the list of "index"
+    if(table->arr[index] == NULL)  //there are no objects in the list of "index"
     {
-        table->arr[index]->next = ptr_new_object;
+        table->arr[index] = ptr_new_object;
         return index;
     }
 
     /*if we got here, there are objects in the list of "index". Need to check if the list is full or not*/
 
     index = check_for_place(table, index);
-    if ( index != -1)  //the new object can be inserted to the list of 'index' (the list is not full)
+    if (index != -1)    //the new object can be inserted to the list of 'index' (the list is not full)
     {
         add_object_to_list(table, ptr_new_object, index);
         return index;
     }
 
-    else   //the new object cannot be inserted to the list because it's full. need to duplicate the table
+    else    //the new object cannot be inserted to the list because it's full. need to duplicate the table
     {
         table->arr = (Object**)realloc(table->arr, sizeof(Object*)*table->current_size*2); //duplicate the size of the hash table
         if (table->arr == NULL)
-            return -1;
+            return ERR;
 
         int i;
         for (i = table->current_size; i < (table->current_size*2); i++)
-            table->arr[i] = createObject(NULL);
+        {
+            table->arr[i] = NULL;
+        }
+
 
         /*need to move all the pointers of the object which where on the hash table before the realloc to their new location after the realloc*/
         int new_index = 0;
 
-
         for (i = table->current_size-1; i > 0 ; i--)
         {
-//                if (i%table->d == 0)
-//                {
-//                    new_index = i * 2;  //the new index for the objects from the old array to be putted in the new array
-//                    int j;
-//                    for (j = i; j < i + table->d; j++)
-//                    {
-//                        table->arr[new_index]->next = table->arr[j]->next; //make the new index point to the objects in the list (if there are objects)
-//                        new_index++;
-//                        if (new_index != j) //for the case that i=0 so also new_index = 0
-//                            table->arr[j]->next = NULL;
-//                    }
-//                }
             new_index = i * 2;
-            table->arr[new_index]->next = table->arr[i]->next; //make the new index point to the objects in the list (if there are objects)
-            if (new_index != i) //for the case that i=0 so also new_index = 0
-                table->arr[i]->next = NULL;
+            table->arr[new_index] = table->arr[i];    //make the new index point to the objects in the list (if there are objects)
+            if (new_index != i) //for the case that i=0 so also new_index = 0, we don't want to lose that pointer
+            {
+                table->arr[i] = NULL;
+            }
         }
         table->d *= 2;  //update the d attribute
         if (table->dType == STR_TYPE)
@@ -215,9 +219,9 @@ int add(Table* table, void* data)
         else   //  (table->>dType == INT_TYPE)
             index = table->d * intHashFun((int*)data, table->original_size);
         index = check_for_place(table, index);   //get the real index of the list where the new object will be
-        add_object_to_list(table, ptr_new_object, index);   //should return the index that points to the list
-        table->current_size *= 2;   //update the size attribute
-        return index;    //j here is the index of the list in the table, where the new object was inserted
+        add_object_to_list(table, ptr_new_object, index);
+        table->current_size *= 2;   //update the current_size attribute
+        return index;
     }
 }
 
@@ -238,7 +242,7 @@ int isEqual(int type, void* data1, void* data2)
         if((*(int*)data1) == (*(int*)data2))
             return 0;
         else
-            return (*(int*)data1)-(*(int*)data2); //return the difference between the 2 object
+            return (*(int*)data1)-(*(int*)data2); //return the ASCII difference between the 2 objects
     }
     else    //it's STR_TYPE
         return strcmp((char*)data1,(char*)data2);
@@ -259,10 +263,16 @@ Object* search(Table* table, void* data)
         return NULL;
     }
 
+    if (data == NULL)
+    {
+        perror("Error: try to find a NULL data in the table\n");
+        return NULL;
+    }
+
     int index = 0;
     if (table->dType == STR_TYPE)
         index = table->d * strHashFun((char*)data, table->original_size);
-    else
+    else    //table->dType == INT_TYPE
         index = table->d * intHashFun((int*)data, table->original_size);
 
     int i;
@@ -307,13 +317,13 @@ int removeObj(Table* table, void* data)
     if (table == NULL)
     {
         perror("Error: The input of the list length was negative\n");
-        return -1;
+        return ERR;
     }
 
     int index = 0;
     if (table->dType == STR_TYPE)
         index = table->d * strHashFun((char*)data, table->original_size);
-    else
+    else   //table->dType == INT_TYPE
         index = table->d * intHashFun((int*)data, table->original_size);
 
     int i;
@@ -321,17 +331,29 @@ int removeObj(Table* table, void* data)
     {
         Object* temp = table->arr[index+i];
         Object* prev_temp = table->arr[index+i];
+        int listLength = list_length(temp);
 
         while (temp != NULL)
         {
             if (isEqual(table->dType, data, temp->data) == 0)
             {
-                prev_temp->next = temp->next;
+                if (listLength > 1)  //in case we remove an object from a list that has more objects than only the 1 we remove
+                {
+                    if (table->arr[index+i] == temp)   //its the first object in the list
+                        table->arr[index+i] = temp->next;
+                    else                               //its not the first object in the list
+                        prev_temp->next = temp->next;
+                }
+
                 freeObject(temp, table->dType);
+
+                if (listLength == 1)  //in case we remove an object from a list of size 1
+                {
+                    table->arr[index+i] = NULL;
+                }
 
                 return (index+i);
             }
-
             else
             {
                 prev_temp = temp;
@@ -339,14 +361,14 @@ int removeObj(Table* table, void* data)
             }
         }
     }
-    return -1; // we did not found the data that needs to be removed
+    return -1; // the object we try to remove from the table does not exist in the table
 }
 
 
 /**
 * The function print the table (the format is in the exe definition)
 */
-void printTable(Table* table)  //TODO prints only int for now
+void printTable(Table* table)
 {
     if (table == NULL)
     {
@@ -354,17 +376,16 @@ void printTable(Table* table)  //TODO prints only int for now
         return;
     }
 
-
     int i;
     for (i = 0; i < table->current_size ; i++)
     {
-        Object* temp = table->arr[i]->next; //create a temp object to print with
+        Object* temp = table->arr[i];   //create a temp object to print with
         printf("[%d]\t", i);
         while (temp != NULL)
         {
             if (table->dType == INT_TYPE)
                 printf("%d\t-->\t", *(int*)(temp->data));
-            else
+            else   //table->dType == STR_TYPE
                 printf("%s\t-->\t", (char*)temp->data);
             temp = temp->next;
         }
@@ -405,9 +426,8 @@ void freeTable(Table* table)
     int i;
     for (i = table->current_size-1; i >= 0  ; i--)   //runs on all the indexes and free the objects in their lists
     {
-        freeTable_rec(table, 0, table->arr[i], table->arr[i]);
+        freeTable_rec(table, 0, table->arr[i]);
     }
-
     free(table->arr);
     free(table);
 }
@@ -415,17 +435,12 @@ void freeTable(Table* table)
 
 /**recursive method which gets to the last object in each list of the hash table indexes, and frees from the last object untill the first object
  * in the list**/
-void freeTable_rec(Table* table, int j, Object* current_ptr, Object* prev_ptr)
+void freeTable_rec(Table* table, int j, Object* current_ptr)
 {
+    if (current_ptr == NULL)
+        return;
     if (j < table->listLength && current_ptr->next != NULL)  //in case we still did not reach the last object in the list
-        freeTable_rec(table, j+1, current_ptr->next,current_ptr);
+        freeTable_rec(table, j+1, current_ptr->next);
 
     freeObject(current_ptr,table->dType);  //current_ptr is the last object in the list if we got here
-
-    /*if (current_ptr->next == NULL) //in case we are reached to the last object in the list
-    {
-        prev_ptr->next = NULL;
-        freeObject(current_ptr,table->dType);
-
-    }*/
 }
